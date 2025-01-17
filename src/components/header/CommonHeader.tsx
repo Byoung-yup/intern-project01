4ae1b3180bc5307d2f56db7bb9386ba7d1d66d42
LeeSeongEun <leeseongeun@LeeSeongEunui-MacBookPro.local>
2025-01-18 01:42:17 +0900

    style: 헤더 레이아웃 작업

--- src/components/header/CommonHeader.tsx
+++ src/components/header/CommonHeader.tsx
@@ -1,5 +1,12 @@
 const CommonHeader: React.FC = () => {
-  return <div className="w-[30px] h-[80px] bg-pink-300">dd</div>;
+  return (
+    <div className="w-full h-[60px] px-6 py-2 flex justify-end items-center bg-white">
+      <div className="flex gap-6">
+        <button className="border-none rounded-lg bg-green-500 text-white font-semibold p-2">로그인</button>
+        <button className="border-none rounded-lg bg-green-500 text-white font-semibold p-2">로그아웃</button>
+      </div>
+    </div>
+  );
 };
 
 export default CommonHeader;
