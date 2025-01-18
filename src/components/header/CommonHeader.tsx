909fa005cebb4b30873f90e69eef46eee5a1613e
LeeSeongEun <leeseongeun@LeeSeongEunui-MacBookPro.local>
2025-01-19 00:33:39 +0900

    feat: 해더 라우팅 작업

--- src/components/header/CommonHeader.tsx
+++ src/components/header/CommonHeader.tsx
@@ -1,16 +1,28 @@
 import { Link } from "react-router";
+import useAuthStore from "../../stores/authStore";
 
 const CommonHeader: React.FC = () => {
+  const accessToken = useAuthStore((state) => state.accessToken);
+
   return (
     <div className="w-full h-[60px] px-6 py-2 flex justify-end items-center bg-white">
-      <div className="flex gap-6">
-        <Link to="/signIn" className="border-none rounded-lg bg-green-500 text-white font-semibold p-2">
-          로그인
-        </Link>
-        <Link to="signUp" className="border-none rounded-lg bg-green-500 text-white font-semibold p-2">
-          회원가입
-        </Link>
-      </div>
+      {accessToken ? (
+        <div className="flex gap-6">
+          <Link to="/myPage" className="border-none rounded-lg bg-green-500 text-white font-semibold p-2">
+            마이페이지
+          </Link>
+          <button className="border-none rounded-lg bg-green-500 text-white font-semibold p-2">로그아웃</button>
+        </div>
+      ) : (
+        <div className="flex gap-6">
+          <Link to="/signIn" className="border-none rounded-lg bg-green-500 text-white font-semibold p-2">
+            로그인
+          </Link>
+          <Link to="signUp" className="border-none rounded-lg bg-green-500 text-white font-semibold p-2">
+            회원가입
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
