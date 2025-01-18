2403e3f56990674f2153995b7b738241659aaf37
LeeSeongEun <leeseongeun@LeeSeongEunui-MacBookPro.local>
2025-01-18 23:01:26 +0900

    feat: 회원가입 작업

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import { SignUpForm } from "../../types";
+import { signUp } from "../../api";
+import { useNavigate } from "react-router";
 
 const SignUp: React.FC = () => {
   return (
@@ -9,14 +12,32 @@ const SignUp: React.FC = () => {
 };
 
 const Container: React.FC = () => {
+  const { register, handleSubmit } = useForm<SignUpForm>();
+  let navigate = useNavigate();
+
+  const onSubmit = async (data: SignUpForm) => {
+    try {
+      const successMessage = await signUp(data);
+      alert(successMessage);
+      navigate("/signIn");
+    } catch (error) {
+      alert(error instanceof Error ? error.message : "알 수 없는 오류가 발생했습니다.");
+    }
+  };
+
   return (
-    <form className="py-3 px-4 flex flex-col gap-4 border-[1px] border-black">
+    <form onSubmit={handleSubmit(onSubmit)} className="py-3 px-4 flex flex-col gap-4 border-[1px] border-black">
       <p className="text-center">
         <span className="text-black font-bold text-4xl">회원가입</span>
       </p>
-      <input type="text" placeholder="아이디" className="p-2 border-[1px] border-black" />
-      <input type="password" placeholder="비밀번호" className="p-2 border-[1px] border-black" />
-      <input type="text" placeholder="닉네임" className="p-2 border-[1px] border-black" />
+      <input {...register("id")} type="text" placeholder="아이디" className="p-2 border-[1px] border-black" />
+      <input
+        {...register("password")}
+        type="password"
+        placeholder="비밀번호"
+        className="p-2 border-[1px] border-black"
+      />
+      <input {...register("nickname")} type="text" placeholder="닉네임" className="p-2 border-[1px] border-black" />
       <button className="p-2 bg-black text-white font-bold">가입하기</button>
     </form>
   );
