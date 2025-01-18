246dcf3461af4832308b7e9785ed5eaa8dcf3b5f
LeeSeongEun <leeseongeun@LeeSeongEunui-MacBookPro.local>
2025-01-19 02:17:59 +0900

    feat: 마이페이지 프로필수정 로직 작업

--- src/hooks/user/useUser.ts
+++ src/hooks/user/useUser.ts
@@ -1,8 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getUser } from "../../api";
-import useAuthStore from "../../stores/authStore";
 
 export const useUser = () => {
-  const accessToken = useAuthStore((state) => state.accessToken);
-  return useQuery({ queryKey: ["user"], queryFn: () => getUser(accessToken), enabled: !!accessToken });
+  return useQuery({ queryKey: ["user"], queryFn: getUser });
 };
